f8d4fc69865f89f3a399e2c67c2625a804207e29
Franklin Noriega <zerocreative.f@gmail.com>
2021-06-29 23:01:18 -0400

    fixes

--- app/DB/trading_pool.js
+++ app/DB/trading_pool.js
@@ -1,6 +1,6 @@
 async function checkDiffTradingPool(dbConnection) {
   try {
-    let result = await dbConnection.query('SELECT * FROM trading_pool WHERE BalanceMinusWithdrawals IS NOT NULL AND (UNIX_TIMESTAMP(CURRENT_TIME())-UNIX_TIMESTAMP(Date))/60/60 <4');
+    let result = await dbConnection.query('SELECT * FROM trading_pool WHERE BalanceMinusWithdrawals IS NOT NULL AND (UNIX_TIMESTAMP(CURRENT_TIME())-UNIX_TIMESTAMP(Date))/60/60 <=12 LIMIT 4');
     return result;
   } catch (e) {
     console.log(e);
